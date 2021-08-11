a919ce7493b552df2a287d65eb65bebcb47bfd48
Kaylie Sheehan <kaylie.sheehan@yahoo.com>
2021-08-10 20:58:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
-from flask import Flask, render_template, jsonify, send_from_directory, request
+from flask import Flask, render_template, jsonify, request
 import json
 import pandas as pd
 import numpy as np
-import os
-from os import environ
 import sqlite3 as sql
-import sys
 
 #init app and class
 app = Flask(__name__)
@@ -13,13 +10,17 @@ app = Flask(__name__)
 #initiate memory cache of database
 conn = sql.connect('Data-and-DBs/pokedex.db')
 
-gen_1_6_query = 'SELECT * FROM gens_1_to_6'
-gen_7_query = 'SELECT * FROM gen_7'
-gen_8_query = 'SELECT * FROM gen_8'
+random_forest_results_query = 'SELECT * FROM random_forest_results'
+neural_network_results_query = 'SELECT * FROM neural_network_results'
+logistic_regression_results_query = 'SELECT * FROM logistic_regression_results'
+knn_results_query = 'SELECT * FROM knn_results'
+svc_results_query = 'SELECT * FROM svc_results'
 
-gen_1_to_6 = pd.read_sql(gen_1_6_query, conn)
-gen_7 = pd.read_sql(gen_7_query, conn)
-gen_8 = pd.read_sql(gen_8_query, conn)
+random_forest_results = pd.read_sql(random_forest_results_query, conn)
+neural_network_results = pd.read_sql(neural_network_results_query, conn)
+logistic_regression_results = pd.read_sql(logistic_regression_results_query, conn)
+knn_results = pd.read_sql(knn_results_query, conn)
+svc_results = pd.read_sql(svc_results_query, conn)
 
 conn.close()
 
@@ -28,13 +29,30 @@ conn.close()
 def home():
     return render_template("index.html")
 
-@app.route("/getData", methods=["GET"])
-
 ####################################
 # ADD MORE ENDPOINTS
 ###########################################
+@app.route("/randomTree", methods=["GET"])
+def randomTreeResults():
+    return jsonify(random_forest_results)
 
+@app.route("/neuralNetwork", methods=["GET"])
+def randomTreeResults():
+    return jsonify(neural_network_results)
+
+@app.route("/logisticRegression", methods=["GET"])
+def randomTreeResults():
+    return jsonify(logistic_regression_results)
+
+@app.route("/KNN", methods=["GET"])
+def randomTreeResults():
+    return jsonify(knn_results)
+
+@app.route("/SVC", methods=["GET"])
+def randomTreeResults():
+    return jsonify(svc_results)
 #############################################################
+
 @app.after_request
 def add_header(r):
     """
